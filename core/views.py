d31700b42c8719b7acdce67edf9bca76d77a97b9
WellingtonSI <12ribe@gmail.com>
2024-10-08 13:43:46 -0300

    mudanças na urls e filters

--- core/views.py
+++ core/views.py
@@ -1,14 +1,15 @@
 from rest_framework import generics
 from .models import Livro, Autor, Categoria
 from .serializers import LivroSerializer, AutorSerializer, CategoriaSerializer
-
-
+from core.filters import LivroFilter,CategoriaFilter,AutorFilter
 
 class LivroList(generics.ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
     name = "livro-list"
-  
+    filterset_class = LivroFilter
+    ordering_fields = ('titulo', 'autor', 'categoria', 'publicado_em')
+    
 class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
@@ -18,6 +19,8 @@ class AutorList(generics.ListCreateAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
     name = "autor-list"
+    filterset_class = AutorFilter
+    ordering_fields = ('nome')
     
 class AutorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Autor.objects.all()
@@ -28,6 +31,8 @@ class CategoriaList(generics.ListCreateAPIView):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
     name = "categoria-list"
+    filterset_class = CategoriaFilter
+    ordering_fields = ('nome')
 
 class CategoriaDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Categoria.objects.all()
